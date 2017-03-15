1b05472e354af2b38c2a4c89076fd5541009a63c
Amy Janett <ajanett@mitre.org>
2017-03-15 17:19:25 -0400

    Added installation script for Asterisk

---
+++ asteriskinstall.py
@@ -0,0 +1,43 @@
+import subprocess
+import os.path
+from time import sleep
+import platform
+
+print 'Before running this script, make sure the public/private key-pair for Git \
+ have been added to /root/.ssh/ and their permissions are 600'
+
+sleep(1.5) 
+
+#download Asterisk
+subprocess.call(['wget', 'http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-14.2.1.tar.gz'], cwd = '/usr/src')
+subprocess.call(['tar', '-zxf', 'asterisk-14.2.1.tar.gz'], cwd = '/usr/src')
+
+#install pre-requisites
+subprocess.call(['./contrib/scripts/install_prereq'], cwd = '/usr/src/asterisk-14.2.1')
+
+#install PJSIP and asterisk
+
+subprocess.call(['./configure', '--with-pjproject-bundled'], cwd = '/usr/src/asterisk-14.2.1')
+subprocess.call(['make', '&&', 'make', 'install', '&&', 'make', 'samples'], cwd = '/usr/src/asterisk-14.2.1')
+subprocess.call(['make', 'config'], cwd = '/usr/src/asterisk-14.2.1')
+subprocess.call(['service', 'asterisk', 'start'], cwd = '/usr/src/asterisk-14.2.1')
+
+#add git repo domain name to known_hosts so RSA fingerprint prompt does not pause script
+subprocess.call(['ssh-keyscan', 'github.com', '>>', '~/.ssh/known_hosts'], cwd = '/usr/src/asterisk-14.2.1')
+
+# pull down confi/media files and add to /etc/asterisk and /var/lib/asterisk/sounds, respectively
+subprocess.call(['git', 'clone', '$9'], cwd = '~')
+subprocess.call(['git', 'checkout', 'AD'], cwd = '~/asterisk')
+subprocess.call(['yes', '|', 'cp', '-rf', 'asterisk-configs/*', '/etc/asterisk'], cwd = '~/asterisk')
+subprocess.call(['yes', '|', 'cp', '-rf', 'asterisk-videos-audios/sounds/*', '/var/lib/asterisk/sounds/'], cwd = '~/asterisk')
+
+subprocess.call(['sed', '-i', '-e', '"s/<public_ip>/$1/g"', 'pjsip.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<local_ip>/$2/g"', 'pjsip.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<dialin>/$3/g"', 'extensions.conf', 'pjsip.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<stun_server>/$4/g"', 'rtp.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<crt_file>/$5/g"', 'http.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<crt_key>/$6/g"', 'http.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<ss_crt>/$7/g"', 'pjsip.conf'], cwd = '/etc/asterisk')
+subprocess.call(['sed', '-i', '-e', '"s/<ss_ca_crt>/$8/g"', 'pjsip.conf'], cwd = '/etc/asterisk')
+
+subprocess.call(['service', 'asterisk', 'restart'], cwd = '/etc/asterisk')
