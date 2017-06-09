ef4642e67d18707d3cf1011a70fb7d94189ff468
Amy Janett <ajanett@mitre.org>
2017-06-09 11:53:22 -0400

    Fixed Asterisk install script

--- asteriskinstall.py
+++ asteriskinstall.py
@@ -9,10 +9,10 @@ private_key = raw_input('Enter value for private_key:')
 #crt_file = raw_input('Enter value for crt_file:')
 #crt_key = raw_input('Enter value for crt_key:')
 
-subprocess.call(['wget', 'https://raw.githubusercontent.com/mitrefccace/asterisk/58bb1740f2d3edfbcf5de0751d40097290d76288/scripts/AD_asterisk_install.sh'])
-subprocess.call(['./AD_asterisk_install.sh', '--public-ip', public_ip, '--local-ip', local_ip, 
-                 '--dial-in', dial_in, '--stun-server', stun_server, '--public-key', public_key,
-                 '--private-key', private_key])
+subprocess.call(['wget', 'https://raw.githubusercontent.com/mitrefccace/asterisk/AD/scripts/AD_asterisk_install_script.sh'])
+subprocess.call(['chmod', '+x', 'AD_asterisk_install_script.sh'])
+subprocess.call('./AD_asterisk_install_script.sh' + ' ' + public_ip + ' ' + local_ip + ' ' + dial_in + ' '
+                 + stun_server + ' ' + public_key + ' ' + private_key, shell=True)
 #subprocess.call(['./AD_asterisk_install.sh', '--public-ip', public_ip, '--local-ip', local_ip, 
 #                 '--stun-server', stun_server, '--dial-in', dial_in, '--crt-file', crt_file,
 #                 '--crt-key', crt_key])
